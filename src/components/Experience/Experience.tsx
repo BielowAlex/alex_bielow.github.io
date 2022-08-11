61c15786b74002d1d526213889ad5f5fa4cee94b
BielowAlex <bielow.alex@gmail.com>
2022-08-11 13:39:36 +0300

    update Experience.tsx;

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -21,7 +21,17 @@ const Experience: React.FC = () => {
                         Aperiam exercitationem ipsa optio.
                     </p>
                 </div>
-
+                <div className="experience_list_item _anim_item">
+                    <span className="date">
+                        Oct 2020-Today
+                    </span>
+                    <p className="exp_info">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Ad, animi distinctio dolore eaque earum est molestiae neque nisi
+                        nobis possimus quaerat reiciendis repudiandae sint sunt ut.
+                        Aperiam exercitationem ipsa optio.
+                    </p>
+                </div>
             </div>
         </div>
     );

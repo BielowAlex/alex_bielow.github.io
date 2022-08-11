d20c6e094e25b342a2dabe096ce62134c3a517a6
BielowAlex <bielow.alex@gmail.com>
2022-08-11 15:03:32 +0300

    update Experience.tsx info; update styles;

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -15,15 +15,19 @@ const Experience: React.FC = () => {
                         Oct 2020-Today
                     </span>
                     <p className="exp_info">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Ad, animi distinctio dolore eaque earum est molestiae neque nisi
-                        nobis possimus quaerat reiciendis repudiandae sint sunt ut.
-                        Aperiam exercitationem ipsa optio.
+                        I started my studies at <a href="https://www.umcs.pl"
+                                                   target='_blank'
+                                                   rel="noreferrer"
+                                                   className="link">Maria Składowska Curie University</a> in Lublin.
+                        In the direction of Informatics, mathematics and physics.
+                        Where I study to this day. It was here that
+                        I got basic knowledge about web programming, databases and learned Java.
+                        And began to learn frontend independently.
                     </p>
                 </div>
                 <div className="experience_list_item _anim_item">
                     <span className="date">
-                        Oct 2020-Today
+                        Oct 2021-Today
                     </span>
                     <p className="exp_info">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.

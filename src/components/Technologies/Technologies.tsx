4b1336b1759120b864d66616cf3b69735a6da299
BielowAlex <bielow.alex@gmail.com>
2022-08-16 18:32:17 +0300

    dix adaptive;

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -6,7 +6,7 @@ const Technologies = () => {
         <div className="technologies section">
             <div className="technologies_title">
                 <h2 className="_anim_item">Technologies</h2>
-                <div className="underline _anim_item">
+                <div className="underline ">
                     <div className="line"/>
                 </div>
             </div>

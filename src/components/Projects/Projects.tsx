f525dd249b1f75dc89d3914aa557246cca85e098
BielowAlex <bielow.alex@gmail.com>
2022-09-03 18:02:21 +0300

    update project;

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -37,6 +37,13 @@ const Projects: React.FC<IProps> = ({reference}) => {
             </div>
             <div className="projects_list"
                  style={{height: `${projectsListHeight}px`}}>
+                <Project projectName="AlexFilms"
+                         poster="alex_films.png"
+                         desc="A multi-page movie search site written in the React/Redux/Axios library.
+                                Used the TMDB API.
+                                Sort by genre, search, trailers, sliders."
+                         url="https://alex-films.vercel.app"
+                         gitUrl="https://github.com/BielowAlex/alex_films_public"/>
                 <Project projectName="Sneakers Shop"
                          poster="react_sneakers.png"
                          desc="The online sneakers-shop is written on the library react/Redux/Axios.
@@ -52,14 +59,6 @@ const Projects: React.FC<IProps> = ({reference}) => {
                                 Different categories, sorting of order execution."
                          url="https://react-pizza-xi.vercel.app"
                          gitUrl="https://github.com/BielowAlex/react_pizza"/>
-                <Project projectName="AlexFilms"
-                         poster="alex_films.png"
-                         desc="A multi-page movie search site written in the React/Redux/Axios library.
-                                Used the TMDB API.
-                                Sort by genre, search, trailers, sliders."
-                         url="https://alex-films.vercel.app"
-                         gitUrl="https://github.com/BielowAlex/alex_films_public"/>
-
                 <OtherProject projectName={'Portfolio DN'}
                               poster={'denis_novak.png'}
                               desc={'Landing page written in pure HTML/SCSS/js. Fully adaptive + mobile versions, animation, popup, multilingual.'}
